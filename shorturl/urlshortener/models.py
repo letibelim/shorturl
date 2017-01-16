7118e226e3cea6df54e1054bf4ee833354e689aa
Tibo <thibault.legrand@gmail.com>
2017-01-16 10:14:37 +0100

    url, model and form done

--- shorturl/urlshortener/models.py
+++ shorturl/urlshortener/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class MiniURL(models.Model):
+	longURL = models.URLField(unique=True, verbose_name= 'url to be shortened')
+	code = models.CharField(max_length=10, unique=True)
+	date = models.DateTimeField(auto_now_add=True, verbose_name = "creation date")
+	creator = models.CharField(max_length=80, null=True)
+	counter = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.longURL
--- shorturl/urlshortener/views.py
+++ shorturl/urlshortener/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from urlshortener import MiniURL
+from .forms import MiniURLForm
 
 # Create your views here.
+
+def all_redirections(request):
+	"""display all URL et their shortened code"""
+	urls = MiniURL.objects.all()
+	render(request, 'urlshortener/allurl.html', {'list_of_urls': urls})
+
+def redirection(request, codesent):
+	""" redirect the shortened url to the original one"""
+	
+	url = MiniURL.objects.get(code=codesent)
+	redirect (url.longURL)
+
+def urlform(request):
+	"""display the form"""
+	form = MiniURLForm(request.POST or None)
+
+	if form.is_valid():
+		longURL = form.cleaned_data['longURL']
+		creator = form.cleaned_data['creator']
+
+	
+
