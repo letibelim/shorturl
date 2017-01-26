98d36bc701a77411d6feaa05031ce8533a287e80
Tibo <thibault.legrand@gmail.com>
2017-01-26 09:40:17 +0100

    minor changes (fields order, labels)' git log git log

--- shorturl/urlshortener/forms.py
+++ shorturl/urlshortener/forms.py
@@ -3,9 +3,9 @@ from .models import MiniURL
 
 
 class MiniURLForm(forms.Form):
-	creator = forms.CharField(max_length=80, required=False)
 	longURL = forms.URLField(label="Copy and paste your url")
 	code = forms.CharField(max_length=10, label = "suggested code, you can customize")
+	creator = forms.CharField(max_length=80, required=False, label="creator (optional)")
 
 	def clean_code(self):
 		code = self.cleaned_data['code']
